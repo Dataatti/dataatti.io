e4716e9a5d85faea59b918b31ddbc73f7c06dc6e
Matias <majlap@utu.fi>
2020-12-30 16:47:22 +0200

    Added new logo and email link

--- pages/index.js
+++ pages/index.js
@@ -5,14 +5,15 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Dataatti.io</title>
+        <title>Dataatti</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <img src="dataattilogo.svg" width="100%" />
+        <img src="dataattilogo.svg" width="50%" />
         <h1>Bringing sweet potatoes to the digital era</h1>
         <h2 id="stay-tuned">Stay tuned!</h2>
+        <a href="mailto:hello@dataatti.io">hello@dataatti.io</a>
       </main>
     </div>
   )
