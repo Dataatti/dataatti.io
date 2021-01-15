91a85dd96b43338c4b718a4b29f0b02935e552b6
Aleksi Väisänen <aleksi.vaisanen@elisanet.fi>
2021-01-15 12:18:05 +0200

    remove false information from cookie policy

--- src/pages/[lang]/privacy-policy.jsx
+++ src/pages/[lang]/privacy-policy.jsx
@@ -15,13 +15,6 @@ const PrivacyPolicy = () => {
             <p>We use cookies for a variety of reasons detailed below. Unfortunately in most cases there are no industry standard options for disabling cookies without completely disabling the functionality and features they add to this site. It is recommended that you leave on all cookies if you are not sure whether you need them or not in case they are used to provide a service that you use.</p>
             <p><strong>Disabling Cookies</strong></p>
             <p>You can prevent the setting of cookies by adjusting the settings on your browser (see your browser Help for how to do this). Be aware that disabling cookies will affect the functionality of this and many other websites that you visit. Disabling cookies will usually result in also disabling certain functionality and features of the this site. Therefore it is recommended that you do not disable cookies. This Cookies Policy was created with the help of the <a href="https://www.cookiepolicygenerator.com/cookie-policy-generator/">Cookies Policy Generator from CookiePolicyGenerator.com</a>.</p>
-            <p><strong>The Cookies We Set</strong></p>
-            <ul>
-                <li>
-                    <p>Forms related cookies</p>
-                    <p>When you submit data to through a form such as those found on contact pages or comment forms cookies may be set to remember your user details for future correspondence.</p>
-                </li>
-            </ul>
             <p><strong>Third Party Cookies</strong></p>
             <p>In some special cases we also use cookies provided by trusted third parties. The following section details which third party cookies you might encounter through this site.</p>
             <ul>
